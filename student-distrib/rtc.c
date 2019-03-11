279431712ffef5d53d73cb77ff9ca1d087d54db9
YiFei Zhu <zhuyifei1999@gmail.com>
2019-03-11 04:15:59 -0500

    Apply rtc.c changes from 9d9ff0e after some formatting

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -5,16 +5,25 @@
 
 // some source from https://wiki.osdev.org/RTC
 
-// handle RTC interrupt
+static uint32_t rtc_ret, rtc_irq_count;
 static void rtc_handler(struct intr_info *info) {
-    // diacard register C to we get interrupts again
+    unsigned long flags;
+    cli_and_save(flags);
+
+    // discard register C to we get interrupts again
     NMI_disable_select_register(0xC);
     inb(0x71);
+    rtc_irq_count++;
+
+    NMI_disable_select_register(0x00);
+    rtc_ret = inb(0x71);
+
     NMI_enable();
+    restore_flags(flags);
 
     // TODO: Remove after checkpoint 1. Provided in lib.c
     void test_interrupts(void);
-    test_interrupts();
+    //test_interrupts();
 }
 
 // TODO: Document frequency formula (Hz) = 32768 >> (rate - 1)
@@ -39,7 +48,6 @@ void rtc_change_rate(unsigned char rate) {
     restore_flags(flags);
 }
 
-// initialize RTC
 static void init_rtc() {
     unsigned long flags;
 
@@ -64,3 +72,37 @@ static void init_rtc() {
     rtc_change_rate(15);
 }
 DEFINE_INITCALL(init_rtc, early);
+
+#include "tests.h"
+#if RUN_TESTS
+/* Rtc Test
+ *
+ * Test whether rtc interrupt interval is 500 ms
+ * Inputs: None
+ * Outputs: None
+ * Side Effects: None
+ */
+static void rtc_test() {
+    int prev_time = rtc_ret;
+    int i = 0;
+    int time_passed = 0;
+    int rtc_irq_count_init = rtc_irq_count;
+
+    while (i < 25) {
+        if (rtc_ret != prev_time) {
+            time_passed++;
+            prev_time = rtc_ret;
+        }
+        if ((rtc_irq_count - rtc_irq_count_init) == i) {
+            printf("    time_passed = %d s , rtc_interrupt_count = %d \n", time_passed, i);
+            i++;
+        }
+    }
+
+    int interval = (time_passed * 1000) / (i - 1);
+
+    printf("\n    rtc_interrupt_interval = %d ms\n", interval);
+    TEST_ASSERT(450 < interval && interval < 550);
+}
+DEFINE_TEST(rtc_test);
+#endif
