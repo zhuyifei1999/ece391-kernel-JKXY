d53ede5c8813134bd4cee29a188f25c0352fc312
YiFei Zhu <zhuyifei1999@gmail.com>
2019-03-11 22:00:41 -0500

    More alignments

--- student-distrib/interrupt.c
+++ student-distrib/interrupt.c
@@ -27,51 +27,51 @@ struct intr_action intr_getaction(uint8_t intr_num) {
     extern void (*ISR_ ## intr ## _ ## suffix)(void);       \
     uint32_t addr = (uint32_t)&ISR_ ## intr ## _ ## suffix; \
     struct idt_desc *entry = &idt[intr];                    \
-    entry->offset_15_00    = addr;                          \
-    entry->offset_31_16    = addr >> 16;                    \
-    entry->seg_selector    = KERNEL_CS;                     \
-    entry->size            = 1; /* 32 bit handler */        \
-    entry->dpl             = _dpl;                          \
-    entry->present         = 1;                             \
-    entry->type            = _type;                         \
+    entry->offset_15_00 = addr;                             \
+    entry->offset_31_16 = addr >> 16;                       \
+    entry->seg_selector = KERNEL_CS;                        \
+    entry->size         = 1; /* 32 bit handler */           \
+    entry->dpl          = _dpl;                             \
+    entry->present      = 1;                                \
+    entry->type         = _type;                            \
 } while (0);
 #define init_IDT_entry(intr, _type, _dpl, suffix) _init_IDT_entry(intr, _type, _dpl, suffix)
 
 // this initializer initializes IDT, with the macro defined above
 static void init_IDT() {
-    init_IDT_entry(INTR_EXC_DIVIDE_BY_ZERO_ERROR, IDT_TYPE_TRAP, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_EXC_DEBUG, IDT_TYPE_TRAP, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_EXC_NON_MASKABLE_INTERRUPT, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_EXC_BREAKPOINT, IDT_TYPE_INTERRUPT, USER_DPL, nocode);
-    init_IDT_entry(INTR_EXC_OVERFLOW, IDT_TYPE_TRAP, USER_DPL, nocode);
-    init_IDT_entry(INTR_EXC_BOUND_RANGE_EXCEEDED, IDT_TYPE_TRAP, USER_DPL, nocode);
-    init_IDT_entry(INTR_EXC_INVALID_OPCODE, IDT_TYPE_TRAP, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_EXC_DEVICE_NOT_AVAILABLE, IDT_TYPE_TRAP, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_DIVIDE_BY_ZERO_ERROR,          IDT_TYPE_TRAP,      KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_DEBUG,                         IDT_TYPE_TRAP,      KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_NON_MASKABLE_INTERRUPT,        IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_BREAKPOINT,                    IDT_TYPE_INTERRUPT, USER_DPL,   nocode);
+    init_IDT_entry(INTR_EXC_OVERFLOW,                      IDT_TYPE_TRAP,      USER_DPL,   nocode);
+    init_IDT_entry(INTR_EXC_BOUND_RANGE_EXCEEDED,          IDT_TYPE_TRAP,      USER_DPL,   nocode);
+    init_IDT_entry(INTR_EXC_INVALID_OPCODE,                IDT_TYPE_TRAP,      KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_DEVICE_NOT_AVAILABLE,          IDT_TYPE_TRAP,      KERNEL_DPL, nocode);
     // TODO: make double fault use a seperate TSS with task gate
-    init_IDT_entry(INTR_EXC_DOUBLE_FAULT, IDT_TYPE_INTERRUPT, KERNEL_DPL, hascode);
-    init_IDT_entry(INTR_EXC_COPROCESSOR_SEGMENT_OVERRUN, IDT_TYPE_TRAP, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_EXC_INVALID_TSS, IDT_TYPE_TRAP, KERNEL_DPL, hascode);
-    init_IDT_entry(INTR_EXC_SEGMENT_NOT_PRESENT, IDT_TYPE_TRAP, KERNEL_DPL, hascode);
-    init_IDT_entry(INTR_EXC_STACK_SEGMENT_FAULT, IDT_TYPE_TRAP, KERNEL_DPL, hascode);
-    init_IDT_entry(INTR_EXC_GENERAL_PROTECTION_FAULT, IDT_TYPE_TRAP, KERNEL_DPL, hascode);
-    init_IDT_entry(INTR_EXC_PAGE_FAULT, IDT_TYPE_INTERRUPT, KERNEL_DPL, hascode);
-    init_IDT_entry(INTR_EXC_X87_FLOATING_POINT_EXCEPTION, IDT_TYPE_TRAP, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_EXC_ALIGNMENT_CHECK, IDT_TYPE_TRAP, KERNEL_DPL, hascode);
-    init_IDT_entry(INTR_EXC_MACHINE_CHECK, IDT_TYPE_TRAP, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_EXC_SIMD_FLOATING_POINT_EXCEPTION, IDT_TYPE_TRAP, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_EXC_VIRTUALIZATION_EXCEPTION, IDT_TYPE_TRAP, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_EXC_SECURITY_EXCEPTION, IDT_TYPE_TRAP, KERNEL_DPL, hascode);
+    init_IDT_entry(INTR_EXC_DOUBLE_FAULT,                  IDT_TYPE_INTERRUPT, KERNEL_DPL, hascode);
+    init_IDT_entry(INTR_EXC_COPROCESSOR_SEGMENT_OVERRUN,   IDT_TYPE_TRAP,      KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_INVALID_TSS,                   IDT_TYPE_TRAP,      KERNEL_DPL, hascode);
+    init_IDT_entry(INTR_EXC_SEGMENT_NOT_PRESENT,           IDT_TYPE_TRAP,      KERNEL_DPL, hascode);
+    init_IDT_entry(INTR_EXC_STACK_SEGMENT_FAULT,           IDT_TYPE_TRAP,      KERNEL_DPL, hascode);
+    init_IDT_entry(INTR_EXC_GENERAL_PROTECTION_FAULT,      IDT_TYPE_TRAP,      KERNEL_DPL, hascode);
+    init_IDT_entry(INTR_EXC_PAGE_FAULT,                    IDT_TYPE_INTERRUPT, KERNEL_DPL, hascode);
+    init_IDT_entry(INTR_EXC_X87_FLOATING_POINT_EXCEPTION,  IDT_TYPE_TRAP,      KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_ALIGNMENT_CHECK,               IDT_TYPE_TRAP,      KERNEL_DPL, hascode);
+    init_IDT_entry(INTR_EXC_MACHINE_CHECK,                 IDT_TYPE_TRAP,      KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_SIMD_FLOATING_POINT_EXCEPTION, IDT_TYPE_TRAP,      KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_VIRTUALIZATION_EXCEPTION,      IDT_TYPE_TRAP,      KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_EXC_SECURITY_EXCEPTION,            IDT_TYPE_TRAP,      KERNEL_DPL, hascode);
 
-    init_IDT_entry(INTR_IRQ0, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_IRQ1, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_IRQ2, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_IRQ3, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_IRQ4, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_IRQ5, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_IRQ6, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_IRQ7, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_IRQ8, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
-    init_IDT_entry(INTR_IRQ9, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ0,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ1,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ2,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ3,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ4,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ5,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ6,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ7,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ8,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
+    init_IDT_entry(INTR_IRQ9,  IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
     init_IDT_entry(INTR_IRQ10, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
     init_IDT_entry(INTR_IRQ11, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
     init_IDT_entry(INTR_IRQ12, IDT_TYPE_INTERRUPT, KERNEL_DPL, nocode);
--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -8,87 +8,87 @@
 // lots of sources from: https://stackoverflow.com/q/37618111
 
 static unsigned char lower_keyboard_map[128] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',     /* 9 */
-    '9', '0', '-', '=', '\b',     /* Backspace */
-    '\t',                 /* Tab */
-    'q', 'w', 'e', 'r',   /* 19 */
-    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', /* Enter key */
-    0,                  /* 29   - Control */
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',     /* 39 */
-    '\'', '`',   0,                /* Left shift */
-    '\\', 'z', 'x', 'c', 'v', 'b', 'n',                    /* 49 */
-    'm', ',', '.', '/',   0,                              /* Right shift */
+    0, 27, '1', '2', '3', '4', '5', '6', '7', '8',    /* 9 */
+    '9', '0', '-', '=', '\b',                         /* Backspace */
+    '\t',                                             /* Tab */
+    'q', 'w', 'e', 'r',                               /* 19 */
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',     /* Enter key */
+    0,                                                /* 29 - Control */
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', /* 39 */
+    '\'', '`', 0,                                     /* Left shift */
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n',               /* 49 */
+    'm', ',', '.', '/', 0,                            /* Right shift */
     '*',
-    0,  /* Alt */
-    ' ',  /* Space bar */
-    0,  /* Caps lock */
-    0,  /* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,  /* < ... F10 */
-    0,  /* 69 - Num lock*/
-    0,  /* Scroll Lock */
-    0,  /* Home key */
-    0,  /* Up Arrow */
-    0,  /* Page Up */
+    0,   /* Alt */
+    ' ', /* Space bar */
+    0,   /* Caps lock */
+    0,   /* 59 - F1 key ... > */
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0,   /* < ... F10 */
+    0,   /* 69 - Num lock*/
+    0,   /* Scroll Lock */
+    0,   /* Home key */
+    0,   /* Up Arrow */
+    0,   /* Page Up */
     '-',
-    0,  /* Left Arrow */
+    0,   /* Left Arrow */
     0,
-    0,  /* Right Arrow */
+    0,   /* Right Arrow */
     '+',
-    0,  /* 79 - End key*/
-    0,  /* Down Arrow */
-    0,  /* Page Down */
-    0,  /* Insert Key */
-    0,  /* Delete Key */
-    0,   0,   0,
-    0,  /* F11 Key */
-    0,  /* F12 Key */
-    0,  /* All other keys are undefined */
+    0,   /* 79 - End key*/
+    0,   /* Down Arrow */
+    0,   /* Page Down */
+    0,   /* Insert Key */
+    0,   /* Delete Key */
+    0, 0, 0,
+    0,   /* F11 Key */
+    0,   /* F12 Key */
+    0,   /* All other keys are undefined */
 };
 
 static unsigned char upper_keyboard_map[128] = {
-    0,  27, '!', '@', '#', '$', '%', '^', '&', '*',     /* 9 */
-    '(', ')', '_', '+', '\b',     /* Backspace */
-    '\t',                 /* Tab */
-    'Q', 'W', 'E', 'R',   /* 19 */
-    'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', /* Enter key */
-    0,                  /* 29   - Control */
-    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',     /* 39 */
-    '"', '~',   0,                /* Left shift */
-    '|', 'Z', 'X', 'C', 'V', 'B', 'N',                    /* 49 */
-    'M', '<', '>', '?',   0,                              /* Right shift */
+    0, 27, '!', '@', '#', '$', '%', '^', '&', '*',    /* 9 */
+    '(', ')', '_', '+', '\b',                         /* Backspace */
+    '\t',                                             /* Tab */
+    'Q', 'W', 'E', 'R',                               /* 19 */
+    'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',     /* Enter key */
+    0,                                                /* 29 - Control */
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', /* 39 */
+    '"', '~', 0,                                      /* Left shift */
+    '|', 'Z', 'X', 'C', 'V', 'B', 'N',                /* 49 */
+    'M', '<', '>', '?', 0,                            /* Right shift */
     '*',
-    0,  /* Alt */
-    ' ',  /* Space bar */
-    0,  /* Caps lock */
-    0,  /* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,  /* < ... F10 */
-    0,  /* 69 - Num lock*/
-    0,  /* Scroll Lock */
-    0,  /* Home key */
-    0,  /* Up Arrow */
-    0,  /* Page Up */
+    0,   /* Alt */
+    ' ', /* Space bar */
+    0,   /* Caps lock */
+    0,   /* 59 - F1 key ... > */
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0,   /* < ... F10 */
+    0,   /* 69 - Num lock*/
+    0,   /* Scroll Lock */
+    0,   /* Home key */
+    0,   /* Up Arrow */
+    0,   /* Page Up */
     '-',
-    0,  /* Left Arrow */
+    0,   /* Left Arrow */
     0,
-    0,  /* Right Arrow */
+    0,   /* Right Arrow */
     '+',
-    0,  /* 79 - End key*/
-    0,  /* Down Arrow */
-    0,  /* Page Down */
-    0,  /* Insert Key */
-    0,  /* Delete Key */
-    0,   0,   0,
-    0,  /* F11 Key */
-    0,  /* F12 Key */
-    0,  /* All other keys are undefined */
+    0,   /* 79 - End key*/
+    0,   /* Down Arrow */
+    0,   /* Page Down */
+    0,   /* Insert Key */
+    0,   /* Delete Key */
+    0, 0, 0,
+    0,   /* F11 Key */
+    0,   /* F12 Key */
+    0,   /* All other keys are undefined */
 };
 
 #define LSHIFT_SCANCODE 0x2A
 #define RSHIFT_SCANCODE 0x36
-#define CTRL_SCANCODE 0x1D
-#define ALT_SCANCODE 0x38
+#define CTRL_SCANCODE   0x1D
+#define ALT_SCANCODE    0x38
 #define CAPSLK_SCANCODE 0x3A
 // TODO: NUMLK, SCRLK
 
@@ -182,7 +182,7 @@ static void keyboard_test() {
     unsigned char test_output_lib[10] = {'w', 'r', 'a', 's', 'd', 'W', 'R', 'A', 'S', 'D'};
     bool caps, shift;
 
-    // testcase both shift and caps are 1
+    // test both shift and caps are 1
     shift = 1;
     caps = 1;
     for (i = 0; i < 5; i++) {
@@ -190,14 +190,14 @@ static void keyboard_test() {
     }
     return;
 
-    // testcase both shift and caps are 0
+    // test both shift and caps are 0
     shift = 0;
     caps = 0;
     for (i = 0; i < 5; i++) {
         TEST_ASSERT(scancode_to_char(test_scancode_lib[i], shift, caps) == test_output_lib[i]);
     }
 
-    // testcase shift is 1
+    // test shift is 1
     shift = 1;
     for (i = 0; i < 5; i++) {
         TEST_ASSERT(scancode_to_char(test_scancode_lib[i], shift, caps) == test_output_lib[i+5]);
