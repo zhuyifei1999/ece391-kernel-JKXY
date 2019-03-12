17d344a41ef4d0c8947ffef4142be349e94d9fed
YiFei Zhu <zhuyifei1999@gmail.com>
2019-03-11 22:33:22 -0500

    exception.c: aligning code here as well

--- student-distrib/exception.c
+++ student-distrib/exception.c
@@ -11,24 +11,24 @@
 DEFINE_INITCALL(init_exc_ ## fn, early)
 
 // create stub exception handler
-#define STUB_EXC_HANDLER(fn_name, intr, mnemonic) \
-static void fn_name(struct intr_info *info) { \
-    printf(mnemonic ": 0x%x", info->error_code); \
-    abort(); \
-} \
+#define STUB_EXC_HANDLER(mnemonic, intr, fn_name) \
+static void fn_name(struct intr_info *info) {     \
+    printf(mnemonic ": 0x%x", info->error_code);  \
+    abort();                                      \
+}                                                 \
 DEFINE_EXC_HANDLER(fn_name, intr)
 
 // initialize stub exception handlers
-STUB_EXC_HANDLER(divide_by_zero, INTR_EXC_DIVIDE_BY_ZERO_ERROR, "#DE");
-STUB_EXC_HANDLER(bound_range_exceeded, INTR_EXC_BOUND_RANGE_EXCEEDED, "#BR");
-STUB_EXC_HANDLER(invalid_opcode, INTR_EXC_INVALID_OPCODE, "#UD");
-STUB_EXC_HANDLER(device_not_available, INTR_EXC_DEVICE_NOT_AVAILABLE, "#NM");
-STUB_EXC_HANDLER(invalid_tss, INTR_EXC_INVALID_TSS, "#TS");
-STUB_EXC_HANDLER(segment_not_present, INTR_EXC_SEGMENT_NOT_PRESENT, "#NP");
-STUB_EXC_HANDLER(stack_segment, INTR_EXC_STACK_SEGMENT_FAULT, "#SS");
-STUB_EXC_HANDLER(general_protection_fault, INTR_EXC_GENERAL_PROTECTION_FAULT, "#GP");
-STUB_EXC_HANDLER(page_fault, INTR_EXC_PAGE_FAULT, "#PF");
-STUB_EXC_HANDLER(x87_floating_point_exception, INTR_EXC_X87_FLOATING_POINT_EXCEPTION, "#MF");
-STUB_EXC_HANDLER(alignment_check, INTR_EXC_ALIGNMENT_CHECK, "#AC");
-STUB_EXC_HANDLER(simd_floating_point_exception, INTR_EXC_SIMD_FLOATING_POINT_EXCEPTION, "#XF");
-STUB_EXC_HANDLER(virtualization_exception, INTR_EXC_VIRTUALIZATION_EXCEPTION, "#VE");
+STUB_EXC_HANDLER("#DE", INTR_EXC_DIVIDE_BY_ZERO_ERROR,          divide_by_zero);
+STUB_EXC_HANDLER("#BR", INTR_EXC_BOUND_RANGE_EXCEEDED,          bound_range_exceeded);
+STUB_EXC_HANDLER("#UD", INTR_EXC_INVALID_OPCODE,                invalid_opcode);
+STUB_EXC_HANDLER("#NM", INTR_EXC_DEVICE_NOT_AVAILABLE,          device_not_available);
+STUB_EXC_HANDLER("#TS", INTR_EXC_INVALID_TSS,                   invalid_tss);
+STUB_EXC_HANDLER("#NP", INTR_EXC_SEGMENT_NOT_PRESENT,           segment_not_present);
+STUB_EXC_HANDLER("#SS", INTR_EXC_STACK_SEGMENT_FAULT,           stack_segment);
+STUB_EXC_HANDLER("#GP", INTR_EXC_GENERAL_PROTECTION_FAULT,      general_protection_fault);
+STUB_EXC_HANDLER("#PF", INTR_EXC_PAGE_FAULT,                    page_fault);
+STUB_EXC_HANDLER("#MF", INTR_EXC_X87_FLOATING_POINT_EXCEPTION,  x87_floating_point_exception);
+STUB_EXC_HANDLER("#AC", INTR_EXC_ALIGNMENT_CHECK,               alignment_check);
+STUB_EXC_HANDLER("#XF", INTR_EXC_SIMD_FLOATING_POINT_EXCEPTION, simd_floating_point_exception);
+STUB_EXC_HANDLER("#VE", INTR_EXC_VIRTUALIZATION_EXCEPTION,      virtualization_exception);
