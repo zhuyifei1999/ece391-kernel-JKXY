392484529e2eb8e27bc798739dad66f162bc4dab
Kevin Xie <KevinXie@vpnpool-192-17-150-242.near.illinois.edu>
2019-03-15 19:48:20 -0500

    ece391 block device

---
+++ student-distrib/drivers/ece391block.h
@@ -0,0 +1,20 @@
+#ifndef _ECE391BLOCK_H
+#define _ECE391BLOCK_H
+
+#include "../types.h"
+
+enum seek_whence {
+    SEEK_SET,
+    SEEK_CUR,
+    SEEK_END,
+};
+
+int32_t ece391block_write(int fd, void *buf, uint32_t nbyte);
+int32_t ece391block_read(int fd, void *buf, uint32_t nbyte);
+int32_t ece391block_open(void);
+int32_t ece391block_close(int fd);
+int32_t ece391block_seek(int fd, int32_t offset, enum seek_whence whence);
+
+
+
+#endif
