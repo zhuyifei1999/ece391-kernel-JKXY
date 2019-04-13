dc1867ad36addc3981f3226b1d7751452d4e2f8b
Bill Chen <ximinc2@illinois.edu>
2019-04-12 23:17:21 -0500

    “ata git commit -m “ata

--- student-distrib/block/ata.c
+++ student-distrib/block/ata.c
@@ -6,28 +6,53 @@
 
 static struct ata_data driver_info;
 
+/*
+The suggestion is to read the Status register FIVE TIMES, 
+and only pay attention to the value returned by the last one
+*/
+void io_delay(){
+	int32_t reg_offset = driver_info.ata_base_reg;
+	// 4 consecutive read to implement 400ns delay
+	inb(reg_offset + STATUS_OFF);
+	inb(reg_offset + STATUS_OFF);
+	inb(reg_offset + STATUS_OFF);
+	inb(reg_offset + STATUS_OFF);
+}
+
+static void soft_reset(struct ata_data* dev){
+	int32_t reg_offset = dev->ata_base_reg;
+	outb(CMD_RESET, reg_offset + STATUS_OFF);
+	outb(0, reg_offset + STATUS_OFF);
+	io_delay(dev);
+	char stat = inb(reg_offset + STATUS_OFF);
+	// check if BSY clear and RDY set
+	while(!(stat & STAT_RDY) || (stat & STAT_BSY)){
+		stat = inb(reg_offset + STATUS_OFF);
+	}
+}
+
 static int32_t ata_read(struct file *file, char *buf, uint32_t nbytes){
     int32_t block_count = nbytes%512 ? nbytes/512 + 1 : nbytes/512;
     uint32_t off = file->pos;
-	int32_t reg_offset = driver_info.io_base_reg;
+	int32_t reg_offset = driver_info.ata_base_reg;
     int32_t byte_count = 0;
 
     if(block_count<0){
-        software_reset(&driver_info)
+        software_reset(&driver_info);
     }
 
     if(block_count > 0x3FFFFF)
         return -1;
 
     uint8_t status = inb(reg_offset + STATUS_OFF);
-	if(status | STAT_BSY_BIT || status & STAT_DRQ_BIT){
+	if(status | STAT_BSY || status & STAT_DRQ){
 		soft_reset(&driver_info);
 	}
 
     int32_t lba = driver_info.stLBA + off/512;
 
     for(int i = 0; i < block_count; i++){
-        if(0 != ata_read_28( (lba+i) , buf))
+        if(0 != ata_read_28( (lba+i) , buf ))
             return -1;
         buf+=512;
         byte_count+=512;
@@ -48,8 +73,9 @@ static int32_t initrd_read(struct file *file, char *buf, uint32_t nbytes) {
 }
 
 static int ata_read_28(uint32_t lba, uint8_t* buf, struct ata_data* dev){
-
-	int32_t reg_offset = driver_info.io_base_reg;
+    uint8_t ret_state;
+    int32_t irq_ret;
+	int32_t reg_offset = driver_info.ata_base_reg;
 	int32_t slavebit = driver_info.slave_bit;
 
 	outb(0xE0 | (slavebit << 4) | ((lba >> 24) & 0x0F), reg_offset + DRIVE_HEAD_OFF);
@@ -61,15 +87,22 @@ static int ata_read_28(uint32_t lba, uint8_t* buf, struct ata_data* dev){
 	outb((uint8_t)(lba>>8), reg_offset + LBA_MID_OFF);
 	outb((uint8_t)(lba>>16), reg_offset + LBA_HI_OFF);
 	// send read command
-	outb(CMD_READ_SEC, reg_offset + STATUS_CMD_OFF);
+	outb(CMD_READ_SEC, reg_offset + STATUS_OFF);
 
-    while (!ata_should_read(ata))
+    io_delay();
+    while(irq_ret = ata_should_read()){
+        if(irq_ret == -1)
+            return -1;
+        else if (irq_ret == 1)
+            break;
+
         schedule();
+    }
     current->state = TASK_RUNNING;
-}
 
 
-static void ATA_handler( uint8_t* buf){
+    int16_t* buffer = (int16_t*)buf;
+	// read 512 bytes to buffer
 	asm volatile (
 			"								\n\
 			movl	%1, %%edx				\n\
@@ -81,22 +114,41 @@ static void ATA_handler( uint8_t* buf){
 			: "r"(buffer), "r"(reg_offset)
 			: "%edx", "%ecx", "%edi"
 	);
+
+    io_delay();
+    ret_state = inb(reg_offset + STATUS_OFF);
+	if(ret_state & STAT_DF || ret_state & STAT_ERR)
+		return -1;
+
+	return 0;
+
+}
+
+static int32_t ata_should_read(){
+    uint8_t status = inb(driver_info.ata_base_reg + STATUS_OFF);
+    if(status & STAT_DF || status & STAT_ERR)
+        return -1;
+    return ((!status & STAT_BSY) & (status & STAT_DRQ));
+}
+
+static void ATA_handler( uint8_t* buf){
+    wake_up_process()
 }
 
 static void set_driver_data(struct ata_data* dev_info){
 	// initialize driver data
 	driver_info.slave_bit = dev_info->slave_bit;
-	driver_info.io_base_reg =dev_info->io_base_reg;
+	driver_info.ata_base_reg =dev_info->ata_base_reg;
 	driver_info.prt_size = dev_info->prt_size;
 	driver_info.stLBA = dev_info->stLBA;
 }
 
 static void init_ATA() {
     set_irq_handler(ATA_IRQ_PRIM, &ATA_handler);
-    set_irq_handler(ATA_IRQ_SEC, &ATA_handler);
+   // set_irq_handler(ATA_IRQ_SEC, &ATA_handler);
     driver_info.slave_bit = 0;
-	driver_info.io_base_reg = 0x1F0;
-	driver_info.prt_size = 8388608;
+	driver_info.ata_base_reg = PRIM_DATA_REG;
+	driver_info.prt_size = EIGHT_MB;
 	driver_info.stLBA = 0;
 
 }
--- student-distrib/block/ata.h
+++ student-distrib/block/ata.h
@@ -13,24 +13,43 @@
 #define ATA_PRIM_IRQ	14	// primary bus irq num
 #define ATA_SEC_IRQ		15	// secondary bus irq num
 
-#define STAT_ERR_BIT	0x01	///< status bit err
-#define STAT_DRQ_BIT	0x08	///< status bit drq
-#define STAT_SRV_BIT	0x10	///< status bit srv
-#define STAT_DF_BIT		0x20	///< status bit df
-#define STAT_RDY_BIT	0x40	///< status bit rdy
-#define STAT_BSY_BIT	0x80	///< status bit bsy
-
-
-
+#define PRIM_DATA_REG	0x1F0	///< primary IO port: 0x1F0 - 0x1F7
+#define ERROR_FEAT_OFF	0x1	///< feature/error register
+#define SEC_COUNT_OFF	0x2	///< sector count
+#define SECTOR_NUM_OFF	0x3	///< sector number
+#define LBA_LO_OFF		0x3	///< lba_lo
+#define CYLIND_LOW_OFF	0x4	///< cylinder low
+#define LBA_MID_OFF		0x4	///< lba_mid
+#define CYLIND_HI_OFF	0x5	///< cylinder high
+#define LBA_HI_OFF		0x5	///< lba_hi
+#define DRIVE_HEAD_OFF	0x6	///< drive/head
 #define STATUS_OFF		0x7
+
+#define STAT_ERR	0x01	///< status bit err
+#define STAT_DRQ	0x08	///< status bit drq
+#define STAT_SRV	0x10	///< status bit srv
+#define STAT_DF		0x20	///< status bit df
+#define STAT_RDY	0x40	///< status bit rdy
+#define STAT_BSY	0x80	///< status bit bsy
+
+#define CMD_RESET			0x4		///< reset command
+#define CMD_READ_SEC		0x20	///< sector read command
+#define CMD_READ_SEC_EXT	0x24	///< sector read ext command
+#define CMD_WRITE_SEC		0x30	///< sector write command
+#define CMD_WRITE_SEC_EXT	0x34	///< sector write ext command
+#define CMD_CACHE_FLUSH		0xE7	///< cache flush command
+#define CMD_MASTER_ID		0xA0	///< master identify command
+#define CMD_SLAVE_ID		0xA0	///< slave identify command
+#define CMD_ID				0xEC	///< identify command
+
+#define EIGHT_MB			8388608
 
 struct ata_data {
 	int32_t slave_bit;		///< master/slave
-	int32_t io_base_reg;	///< primary/secondary
+	int32_t ata_base_reg;	///< primary/secondary
 	int32_t stLBA;			///< lba offset
 	int32_t prt_size;		///< partition size
 };
-
 
 
 #endif
