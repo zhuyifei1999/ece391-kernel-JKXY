3d1a8f0bcd3eeec1d3f5a512009ef142a85a55fe
YiFei Zhu <zhuyifei1999@gmail.com>
2019-04-14 01:02:35 -0500

    ATA working :)

--- student-distrib/block/ata.c
+++ student-distrib/block/ata.c
@@ -1,5 +1,6 @@
 #include "../irq.h"
 #include "../lib/stdint.h"
+#include "../lib/stdbool.h"
 #include "../lib/string.h"
 #include "../vfs/file.h"
 #include "../vfs/device.h"
@@ -17,7 +18,8 @@
 #define ATA_SEC_IRQ  15 // secondary bus irq num
 
 #define PRIM_DATA_REG  0x1F0 ///< primary IO port: 0x1F0 - 0x1F7
-#define SEC_DATA_REG   0x170 ///< primary IO port: 0x1F0 - 0x1F7
+#define SEC_DATA_REG   0x170 ///< primary IO port: 0x170 - 0x177
+
 #define ERROR_FEAT_OFF 0x1   ///< feature/error register
 #define SEC_COUNT_OFF  0x2   ///< sector count
 #define SECTOR_NUM_OFF 0x3   ///< sector number
@@ -28,6 +30,8 @@
 #define LBA_HI_OFF     0x5   ///< lba_hi
 #define DRIVE_HEAD_OFF 0x6   ///< drive/head
 #define STATUS_OFF     0x7
+#define COMMAND_OFF    0x7
+#define ALTERNATE_STAT 0x206
 
 #define STAT_ERR 0x01 ///< status bit err
 #define STAT_DRQ 0x08 ///< status bit drq
@@ -45,7 +49,6 @@
 #define CMD_MASTER_ID     0xA0 ///< master identify command
 #define CMD_SLAVE_ID      0xB0 ///< slave identify command
 #define CMD_ID            0xEC ///< identify command
-
 
 #define SECTOR_SIZE      512
 
@@ -80,45 +83,46 @@ The suggestion is to read the Status register FIVE TIMES,
 and only pay attention to the value returned by the last one
 */
 static void io_delay(struct ata_data *dev) {
+
     int32_t reg_offset = dev->ata_base_reg;
     // 4 consecutive read to implement 400ns delay
-    inb(reg_offset + STATUS_OFF);
-    inb(reg_offset + STATUS_OFF);
-    inb(reg_offset + STATUS_OFF);
-    inb(reg_offset + STATUS_OFF);
+    inb(reg_offset + ALTERNATE_STAT);
+    inb(reg_offset + ALTERNATE_STAT);
+    inb(reg_offset + ALTERNATE_STAT);
+    inb(reg_offset + ALTERNATE_STAT);
 }
 
+__attribute__((unused))
 static void soft_reset(struct ata_data *dev) {
     int32_t reg_offset = dev->ata_base_reg;
-    outb(CMD_RESET, reg_offset + STATUS_OFF);
-    outb(0, reg_offset + STATUS_OFF);
+    outb(CMD_RESET, reg_offset + COMMAND_OFF);
+    outb(0, reg_offset + COMMAND_OFF);
     io_delay(dev);
-    char stat = inb(reg_offset + STATUS_OFF);
+    char stat = inb(reg_offset + ALTERNATE_STAT);
     // check if BSY clear and RDY set
     while (!(stat & STAT_RDY) || (stat & STAT_BSY)) {
-        stat = inb(reg_offset + STATUS_OFF);
+        stat = inb(reg_offset + ALTERNATE_STAT);
     }
 }
 
-//__attribute__((unused))
 static int ata_identify(struct ata_data *ata) {
     outb(0xA0, ata->ata_base_reg + DRIVE_HEAD_OFF);
     outb(0, ata->ata_base_reg + SEC_COUNT_OFF);
     outb(0, ata->ata_base_reg + SECTOR_NUM_OFF);
     outb(0, ata->ata_base_reg + LBA_MID_OFF);
     outb(0, ata->ata_base_reg + LBA_HI_OFF);
-    outb(CMD_ID, ata->ata_base_reg + STATUS_OFF);
-    uint8_t stat = inb(ata->ata_base_reg + STATUS_OFF);
+    outb(CMD_ID, ata->ata_base_reg + COMMAND_OFF);
+    uint8_t stat = inb(ata->ata_base_reg + ALTERNATE_STAT);
 
     if (!stat)
         return -ENXIO;
     while (stat & STAT_BSY) {
-        stat = inb(ata->ata_base_reg + STATUS_OFF);
+        stat = inb(ata->ata_base_reg + ALTERNATE_STAT);
     }
     while (!(stat & STAT_DRQ)) {
         if (stat & STAT_ERR)
             return -EIO;
-        stat = inb(ata->ata_base_reg + STATUS_OFF);
+        stat = inb(ata->ata_base_reg + ALTERNATE_STAT);
     }
 
     uint16_t id_buf[256];
@@ -139,25 +143,18 @@ static int ata_identify(struct ata_data *ata) {
 }
 
 static int32_t ata_should_read(struct ata_data *dev) {
-    uint8_t status = inb(dev->ata_base_reg + STATUS_OFF);
+    uint8_t status = inb(dev->ata_base_reg + ALTERNATE_STAT);
     if (status & STAT_DF || status & STAT_ERR)
         return -EIO;
-    return ((~status & STAT_BSY) && (status & STAT_DRQ));
+    return (!(status & STAT_BSY) && (status & STAT_DRQ));
 }
 
 static int ata_read_28(uint32_t lba, char *buf, struct ata_data *dev) {
     uint8_t ret_state;
-    int32_t irq_ret;
     int32_t reg_offset = dev->ata_base_reg;
     int32_t slavebit = dev->slave_bit;
 
-    list_insert_back(&ata_queue, current);
-    current->state = TASK_UNINTERRUPTIBLE;
-    while (list_peek_front(&ata_queue) != current) {
-        schedule();
-    }
-    current->state = TASK_RUNNING;
-
+    cli();
     outb(0xE0 | (slavebit << 4) | ((lba >> 24) & 0x0F), reg_offset + DRIVE_HEAD_OFF);
     outb(0x00, reg_offset + ERROR_FEAT_OFF);
     // write sector count to port
@@ -167,24 +164,26 @@ static int ata_read_28(uint32_t lba, char *buf, struct ata_data *dev) {
     outb((uint8_t)(lba>>8), reg_offset + LBA_MID_OFF);
     outb((uint8_t)(lba>>16), reg_offset + LBA_HI_OFF);
     // send read command
-    outb(CMD_READ_SEC, reg_offset + STATUS_OFF);
+    outb(CMD_READ_SEC, reg_offset + COMMAND_OFF);
+    sti();
 
     io_delay(dev);
 
-    int32_t ret = 0;
-
-    while (irq_ret == ata_should_read(dev)) {
-        if (irq_ret < 0) {
-            ret = irq_ret;
-            goto out;
-        } else if (irq_ret)
+    while (true) {
+        int32_t irq_ret = ata_should_read(dev);
+        if (irq_ret < 0)
+            return irq_ret;
+        else if (irq_ret)
             break;
-        current->state = TASK_UNINTERRUPTIBLE;
-        schedule();
-        current->state = TASK_RUNNING;
+
+        // current->state = TASK_UNINTERRUPTIBLE;
+        // schedule();
+        // current->state = TASK_RUNNING;
     }
 
-    // read SECTOR_SIZE bytes to buffer
+    io_delay(dev);
+
+    // read sector to buffer
     asm volatile (
         "               \n\
         movl %1,%%edx   \n\
@@ -198,80 +197,75 @@ static int ata_read_28(uint32_t lba, char *buf, struct ata_data *dev) {
     );
 
     io_delay(dev);
-    ret_state = inb(reg_offset + STATUS_OFF);
-    if (ret_state & STAT_DF || ret_state & STAT_ERR) {
-        ret = -EIO;
-        goto out;
-    }
+    ret_state = inb(reg_offset + ALTERNATE_STAT);
+    if (ret_state & STAT_DF || ret_state & STAT_ERR)
+        return -EIO;
 
-out:
-    cli();
-    if (list_peek_front(&ata_queue) != current)
-        BUG();
-    list_pop_front(&ata_queue);
-    sti();
-
-    return ret;
+    return 0;
 }
 
 static int32_t ata_read(struct file *file, char *buf, uint32_t nbytes) {
     struct ata_data *ata = file->vendor;
-    uint32_t off = file->pos;
-    int32_t reg_offset = ata->ata_base_reg;
-    int32_t byte_count = 0;
 
-    uint8_t status = inb(reg_offset + STATUS_OFF);
-    if (status | STAT_BSY || status & STAT_DRQ) {
-        soft_reset(ata);
-    }
-
-    uint32_t sector_start = off / SECTOR_SIZE;
-    uint32_t sector_end = (nbytes + off) / SECTOR_SIZE;
-
-    if (sector_start > sector_end || sector_end > 0xFFFFFF)
+    if (nbytes > ata->prt_size - file->pos)
+        nbytes = ata->prt_size - file->pos;
+    if (!nbytes)
         return 0;
 
     char *read_head_buf = kmalloc(SECTOR_SIZE);
+    if (!read_head_buf)
+        return -ENOMEM;
 
-    int32_t res;
+    list_insert_back(&ata_queue, current);
+    current->state = TASK_UNINTERRUPTIBLE;
+    while (list_peek_front(&ata_queue) != current) {
+        schedule();
+    }
+    current->state = TASK_RUNNING;
+
+    // int32_t reg_offset = ata->ata_base_reg;
+    // uint8_t status = inb(reg_offset + ALTERNATE_STAT);
+    // if (status | STAT_BSY || status & STAT_DRQ) {
+    //     soft_reset(ata);
+    // }
+
+    int32_t byte_count = 0;
     int32_t ret;
-    res = ata_read_28(sector_start, read_head_buf, ata);
-    if (res) {
-        ret = res;
-        goto out;
-    }
-    if (sector_start == sector_end) {
-        memcpy(buf, read_head_buf + off % SECTOR_SIZE, nbytes);
-        ret = nbytes;
-        goto out;
-    }
+    while (nbytes) {
+        uint32_t sector_num = file->pos / SECTOR_SIZE;
+        uint32_t sector_off = file->pos % SECTOR_SIZE;
+        ret = ata_read_28(sector_num, read_head_buf, ata);
 
-    memcpy(buf, read_head_buf + off % SECTOR_SIZE, SECTOR_SIZE - off % SECTOR_SIZE);
-    buf += SECTOR_SIZE - off % SECTOR_SIZE;
-    byte_count += SECTOR_SIZE - off % SECTOR_SIZE;
-    for (; sector_start < sector_end; sector_start++) {
-        res = ata_read_28(sector_start, read_head_buf, ata);
-        if (res) {
-            ret = res;
+        if (ret)
             goto out;
-        }
-        buf += SECTOR_SIZE;
-        byte_count += SECTOR_SIZE;
-    }
 
-    res = ata_read_28(sector_start, read_head_buf, ata);
-    if (res) {
-        ret = res;
-        goto out;
-    }
+        uint32_t inner_nbytes = SECTOR_SIZE - sector_off;
+        if (inner_nbytes > nbytes)
+            inner_nbytes = nbytes;
 
-    memcpy(buf, read_head_buf, (nbytes + off) % SECTOR_SIZE);
-    byte_count += (nbytes + off) % SECTOR_SIZE;
+        memcpy(buf, read_head_buf + sector_off, inner_nbytes);
+
+        nbytes -= inner_nbytes;
+        byte_count += inner_nbytes;
+        buf += inner_nbytes;
+        file->pos += inner_nbytes;
+    }
 
     ret = byte_count;
 
 out:
     kfree(read_head_buf);
+
+    cli();
+    if (list_peek_front(&ata_queue) != current)
+        BUG();
+    list_pop_front(&ata_queue);
+
+    // Wake up the next in queue
+    if (!list_isempty(&ata_queue))
+        wake_up_process(list_peek_front(&ata_queue));
+    sti();
+
     return ret;
 }
 
@@ -323,13 +317,14 @@ static struct file_operations ata_dev_op = {
     .seek    = &ata_seek,
 };
 
+__attribute__((unused))
 static void ata_handler() {
     wake_up_process(list_peek_front(&ata_queue));
 }
 
 static void ata_init() {
-    set_irq_handler(ATA_IRQ_PRIM, &ata_handler);
-    set_irq_handler(ATA_IRQ_SEC, &ata_handler);
+    // set_irq_handler(ATA_IRQ_PRIM, &ata_handler);
+    // set_irq_handler(ATA_IRQ_SEC, &ata_handler);
 
     // ATA has major device number 8
     register_dev(S_IFBLK, MKDEV(8, MINORMASK), &ata_dev_op);
