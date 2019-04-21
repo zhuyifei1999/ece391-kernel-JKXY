812cdacc316d1e6777d7e96682d38a97895af2c2
YiFei Zhu <zhuyifei1999@gmail.com>
2019-04-21 14:39:24 -0500

    Add time/
    Missing in last commits.

---
+++ student-distrib/time/time.c
@@ -0,0 +1,43 @@
+#include "../drivers/rtc.h"
+#include "../lib/stdint.h"
+#include "../mm/paging.h"
+#include "../syscall.h"
+
+// source: kernel/time/time.c
+uint64_t mktime64(const unsigned int year0, const unsigned int mon0,
+        const unsigned int day, const unsigned int hour,
+        const unsigned int min, const unsigned int sec)
+{
+    unsigned int mon = mon0, year = year0;
+
+    /* 1..12 -> 11,12,1..10 */
+    if (0 >= (int) (mon -= 2)) {
+        mon += 12;    /* Puts Feb last since it has leap day */
+        year -= 1;
+    }
+
+    return ((((uint64_t)
+          (year/4 - year/100 + year/400 + 367*mon/12 + day) +
+          year*365 - 719499
+        )*24 + hour /* now have hours - midnight tomorrow handled here */
+      )*60 + min /* now have minutes */
+    )*60 + sec; /* finally seconds */
+}
+
+DEFINE_SYSCALL1(LINUX, time, uint64_t *, tloc) {
+    struct rtc_timestamp timestamp;
+    rtc_get_timestamp(&timestamp);
+    uint64_t time = mktime64(
+        timestamp.year,
+        timestamp.month,
+        timestamp.day,
+        timestamp.hour,
+        timestamp.minute,
+        timestamp.second
+    );
+
+    if (safe_buf(tloc, sizeof(*tloc), true) == sizeof(*tloc))
+        *tloc = time;
+
+    return time;
+}
---
+++ student-distrib/time/timer.c
@@ -0,0 +1,61 @@
+#include "timer.h"
+#include "../lib/cli.h"
+#include "../drivers/rtc.h"
+#include "../initcall.h"
+
+static uint32_t last_calibration = rtc_rate_to_freq(RTC_HW_RATE);
+static uint32_t rtc_counter = 0;
+static uint32_t seconds = 0;
+static uint16_t last_second = -1;
+
+static void rtc_handler() {
+    // This is an FSM:
+    // State 0 = initial loaded, last_second undefined
+    // State 1 = waiting for seconds align
+    // State 2 = normal mode
+    static uint8_t state = 0;
+    uint8_t second = rtc_get_second();
+    switch (state) {
+    case 0:
+        state++;
+        break;
+    case 1:
+        if (second != last_second) {
+            state++;
+            rtc_counter = 0;
+        }
+    case 2:
+        if (second != last_second) {
+            last_calibration = rtc_counter;
+            if (!last_calibration)
+                last_calibration = 1;
+
+            seconds++;
+            rtc_counter = 0;
+        }
+    }
+    last_second = second;
+    rtc_counter++;
+}
+
+void get_timer(struct timer_data *data) {
+    unsigned long flags;
+    cli_and_save(flags);
+
+    // Make sure millis can never go above 1000
+    uint32_t counter = rtc_counter;
+    if (counter >= last_calibration)
+        counter = last_calibration - 1;
+
+    *data = (struct timer_data) {
+        .seconds = seconds,
+        .millis = counter * 1000 / last_calibration
+    };
+
+    restore_flags(flags);
+}
+
+static void init_timer() {
+    register_rtc_handler(&rtc_handler);
+}
+DEFINE_INITCALL(init_timer, drivers);
