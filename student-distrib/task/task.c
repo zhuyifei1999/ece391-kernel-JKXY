270c477f0a036ff9d87816b5cb6704f74fc1f0b7
Kevin Xie <KevinXie@wirelessprv-10-192-114-174.near.illinois.edu>
2019-04-26 17:36:22 -0500

    comments for task.c

--- student-distrib/task/task.c
+++ student-distrib/task/task.c
@@ -7,10 +7,14 @@
 struct list tasks;
 LIST_STATIC_INIT(tasks);
 
+// get the pid of a process
 struct task_struct *get_task_from_pid(uint16_t pid) {
+    // check if pid exceeds the upper limit
     if (pid > MAXPID)
         return ERR_PTR(-ESRCH);
 
+    // check each element in the task list and find the one that matches the
+    // specified pid
     struct list_node *node;
     list_for_each(&tasks, node) {
         struct task_struct *task = node->value;
@@ -18,6 +22,7 @@ struct task_struct *get_task_from_pid(uint16_t pid) {
             return task;
     }
 
+    // not found
     return ERR_PTR(-ESRCH);
 }
 
